1eece691985ab00fea9d44af990cad240b3ff66a
Varunkhatake <varunkhatake7777@gmail.com>
2024-01-30 23:40:02 +0530

    program1.py

---
+++ assignments/assignment2/program1.py
@@ -0,0 +1,10 @@
+"WAP to check num are divisible by 4 and 5"
+
+num = int(input("enter num: "))
+
+if(num%4==0 and num%5==0):
+    print(num,"is divided by 4 & 5")
+else:
+    print(num,"is not divided by 4 & 5")
+
+
